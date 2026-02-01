2efa6b4be83eafb9e13b1ef9d550ce45c9a81328
Talal Alomayri <alomayritalal@gmail.com>
2026-02-01 15:04:26 +0300

    Adding 3 components

---
+++ src/ButtonsOnAbov.jsx
@@ -0,0 +1,13 @@
+export default function ButtonsOnAbov(){
+    return(
+         <div className=" mb-5 md:flex md:justify-between lg:pt-3 ">
+           <div className="font-bold text-center text-Heads text-4xl
+                          md:ml-10 md:text-2xl lg:ml-30 ">Extensions List</div>
+             <div className=" text-xl text-center md:mr-10 md:text-sm lg:mr-30  ">
+              <button className=" bg-slate-700  text-Heads border border-gray-500  rounded-3xl hover:bg-orange-600 enabled:hover:bg-orange-600 pl-5 md:pl-3 pr-5 md:pr-3 pb-2 lg:pt-1 mt-6 ml-2 mr-1 mb-1 md:mt-1">All</button>
+              <button className=" bg-slate-700  text-Heads border border-gray-500  rounded-3xl hover:bg-orange-600 pl-5 md:pl-3 pr-5 md:pr-3 pb-2 lg:pt-1 mt-6 ml-2 mr-1 mb-1 md:mt-1">Active</button>
+              <button className=" bg-slate-700  text-Heads border border-gray-500  rounded-3xl hover:bg-orange-600 pl-5 md:pl-3 pr-5 md:pr-3 pb-2 lg:pt-1 mt-6 ml-2 mr-1 mb-1 md:mt-1">Inactie</button>
+            </div>
+         </div>
+    );
+}
